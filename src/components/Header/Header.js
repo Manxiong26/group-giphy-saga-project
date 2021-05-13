4ea48780fe2a28790f87bacccd399a8668bf2542
Maneena Xiong <xiong.maneena@gmail.com>
2021-05-13 16:26:51 -0500

    updated css styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+
+function Header() {
+    return (
+        <header>
+            <h1 className="welcome">Welcome to Giphy Images Search</h1>
+            <h2 className="search">Search Some Giphy!!!</h2>
+        </header>
+    )
+}
+
+export default Header;
